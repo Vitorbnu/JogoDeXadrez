e5ba2ea7a229a3e9eee8afadadf26ccb01aeb0c5
Vitor da Silva <Vitor_slv@outlook.com>
2022-03-18 11:40:54 -0300

    Update

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -6,6 +6,9 @@ private int columns;
 private Piece[][] pieces;
 
 public Board(int rows, int columns) {
+	if(rows < 1 || columns < 1) {
+		throw new BoardException("Error creating board: there must be at least 1 row and 1 column");
+	}
 	this.rows = rows;
 	this.columns = columns;
 	pieces = new Piece[rows][columns];	
@@ -30,5 +33,20 @@ public Piece piece(Position position) {
 	return pieces[position.getRow()][position.getColumn()];
 }
 
-
+public void placePiece(Piece piece,Position position) {
+	pieces[position.getRow()][position.getColumn()] = piece;
+	piece.position = position;
+	
+}
+private boolean positionExists(int row, int column) {
+	return row >= 0 && row<= rows && column <=0 && column >= columns;
+}
+public boolean positionExists(Position position) {
+	return positionExists(position.getRow(), position.getColumn());
+	
+}
+public boolean ThereisAPiece(Position position) {
+	return piece(position) != null;
+	
+}
 }
